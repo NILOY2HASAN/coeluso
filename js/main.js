f62299545fe936146a6823b74a2afb8c529e34d4
Niloy hasan <56444989+NILOY2HASAN@users.noreply.github.com>
2023-09-29 08:23:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".artist_carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    smartSpeed: 1000,
+  });
+});
